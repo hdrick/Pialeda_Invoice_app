6bc1da642e145fbbca798c38231efe5616ae89bb
hendwrick <hendwrick.gonzales@ext.essilor.ph>
2023-03-27 19:57:00 +0800

    Added auto generated default admin

--- src/main/java/pialeda/app/Invoice/InvoiceManagementApplication.java
+++ src/main/java/pialeda/app/Invoice/InvoiceManagementApplication.java
@@ -1,13 +1,34 @@
 package pialeda.app.Invoice;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import pialeda.app.Invoice.model.User;
+import pialeda.app.Invoice.repository.UserRepository;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class InvoiceManagementApplication {
+	@Autowired
+	private UserRepository userRepository;
 
+	@Autowired
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
 	public static void main(String[] args) {
 		SpringApplication.run(InvoiceManagementApplication.class, args);
 	}
-
+	@PostConstruct
+	public void init() {
+		if (userRepository.count() == 0) {
+			User adminUser = new User();
+			adminUser.setRole("admin");
+			adminUser.setPassword(bCryptPasswordEncoder.encode("adminpassword"));
+			adminUser.setEmail("admin@gmail.com");
+			adminUser.setFirstName("admin");
+			adminUser.setLastName("admin");
+			userRepository.save(adminUser);
+		}
+	}
 }
